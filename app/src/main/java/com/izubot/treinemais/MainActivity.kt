5b6be0fb14424c3ed874e6ef742a1f10d6ca1593
Kauan Cardoso <kauanmartins977@gmail.com>
2026-01-29 13:43:42 -0300

    Adiçao da biblioteca de icones do Google

--- app/src/main/java/com/izubot/treinemais/MainActivity.kt
+++ app/src/main/java/com/izubot/treinemais/MainActivity.kt
@@ -8,8 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.izubot.treinemais.ui.splash.Splash
 import com.izubot.treinemais.ui.theme.TreineMaisTheme
+import com.izubot.treinemais.ui.welcome.Welcome
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TreineMaisTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Splash(modifier = Modifier.padding(innerPadding))
+                    Welcome(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
