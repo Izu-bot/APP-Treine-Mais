6db776c697e1ed5c0d7ee08cd3189358803a5b47
Kauan Cardoso <kauanmartins977@gmail.com>
2026-06-01 18:46:22 -0300

    refact: remover dados hardcoded de ProfileUiState e limpar imports

--- app/src/main/java/com/izubot/treinemais/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/izubot/treinemais/ui/register/RegisterViewModel.kt
@@ -1,7 +1,6 @@
 package com.izubot.treinemais.ui.register
 
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.izubot.treinemais.R
