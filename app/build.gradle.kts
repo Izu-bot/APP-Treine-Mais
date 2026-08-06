ee4391a0d3cb1d94b787e6279d445e154593bd68
Kauan Cardoso <kauanmartins977@gmail.com>
2026-08-06 20:27:43 -0300

    versão 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.izubot.treinemais"
         minSdk = 28
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
